72143d5ee6bb1443feb763dde73e62040c423d0a
wjdan1778 <hdhsb6jgn9@privaterelay.appleid.com>
2026-09-13 20:40:34 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='maayir-copc-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(self.location.pathname.replace(/sw\.js$/,'')?[]:[]))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
