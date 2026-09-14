9d41129407d97c4a5676398272113d9d553e02b7
wjdan1778 <hdhsb6jgn9@privaterelay.appleid.com>
2026-09-14 10:43:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,1 @@
-self.addEventListener('install',function(e){self.skipWaiting();});
-self.addEventListener('activate',function(e){e.waitUntil(self.registration.unregister());});
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.registration.unregister()));
